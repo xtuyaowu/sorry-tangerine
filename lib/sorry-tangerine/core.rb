c3809b0a0a5d721e284363f9def3c884e9454973
zgs225 <zgs225@gmail.com>
2016-03-04 19:01:09 +0800

    优化抓取逻辑

--- lib/sorry-tangerine/core.rb
+++ lib/sorry-tangerine/core.rb
@@ -14,7 +14,12 @@ module SorryTangerine
       Spidr.user_agent = UserAgentRandomizer::UserAgent.fetch(type: "desktop_browser").string
       Spidr.robots     = false
 
-      Spidr.start_at("http://www.itjuzi.com/company") do |spidr|
+      links = [
+        /http:\/\/www\.itjuzi\.com\/company\?page=\d+$/,
+        /http:\/\/www\.itjuzi\.com\/company\/\d+$/
+      ];
+
+      Spidr.start_at("http://www.itjuzi.com/company?page=1", links: links) do |spidr|
         pattern = /^http:\/\/.+company\/\d+$/
 
         spidr.every_url do |url|
@@ -28,15 +33,19 @@ module SorryTangerine
         end
 
         spidr.every_url_like(pattern) do |url|
-          page = spidr.get_page url
+          unless url.to_s =~ /redirect=company/
+            page = spidr.get_page url
 
-          parser = SorryTangerine::Parser.new page
-          parser.parse
-          parser.store_as_json
+            parser = SorryTangerine::Parser.new page
+            parser.parse
+            parser.store_as_json
 
-          puts "\n"
-          puts parser.data
-          puts "\n[#{Time.now}] 解析结束：#{page.url}\n"
+            puts "\n"
+            puts parser.data
+            puts "\n[#{Time.now}] 解析结束：#{page.url}\n"
+          else
+            puts "[#{Time.now}] 跳过解析：#{url}\n"
+          end
         end
       end
     end
